cfbc0296b59e5ffd66ae2f42353f6bb89ef5649c
JYLee1206 <dlwnsdud1206@naver.com>
2024-05-09 13:30:20 +0900

    기본 형식

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const pageRouter = require('./routes/page');
 
 const app = express();
 app.set('port', process.env.PORT || 8001);
@@ -31,22 +30,3 @@ app.use(session({
     secure: false,
   },
 }));
-
-app.use('/', pageRouter);
-
-app.use((req, res, next) => {
-  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-  error.status = 404;
-  next(error);
-});
-
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기중');
-});
