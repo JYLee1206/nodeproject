8a1ad65d63eb89b702c7da05738f9576a3a9bdf6
JYLee1206 <dlwnsdud1206@naver.com>
2024-05-27 10:51:53 +0900

    DB 에러 추가

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ app.use(session({
 mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB 연결 성공'))
     .catch(err => console.error('MongoDB 연결 실패', err));
+// 에러 및 연결 끊김 이벤트 리스너
+mongoose.connection.on('error', (error) => {
+    console.error('몽고디비 연결 에러', error);
+  });
+  
+  mongoose.connection.on('disconnected', () => {
+    console.error('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.');
+    connect();
+  });
 
 // 뷰 엔진 설정
 nunjucks.configure('views', {
